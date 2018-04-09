444dae49c3741f2f79134f2264d67097f9beec10
fangcun <fangcun010@gmail.com>
2018-04-09 19:34:47 +0800

    Update

---
+++ SimpleAudio.h
@@ -0,0 +1,7 @@
+#ifndef SIMPLEAUDIO_H_INCLUDED
+#define SIMPLEAUDIO_H_INCLUDED
+
+#include <AL/al.h>
+#include <AL/alut.h>
+
+#endif // SIMPLEAUDIO_H_INCLUDED
---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "SimpleAudio.h"
+
+using namespace std;
+
+int main()
+{
+    return 0;
+}
