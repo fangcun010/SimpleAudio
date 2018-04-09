c3726fc2446c817b7fd0f13c5102829a21b933b0
fangcun <fangcun010@gmail.com>
2018-04-09 20:04:19 +0800

    Update

--- SimpleAudio.c
+++ SimpleAudio.c
@@ -1,2 +1,57 @@
 #include "SimpleAudio.h"
 
+ALuint saCreateAudioSource()
+{
+    ALuint source;
+
+    alGenSources(1,&source);
+
+    alSource3f(source,AL_POSITION,0,0,0);
+	alSource3f(source,AL_VELOCITY,0,0,0);
+	alSource3f(source,AL_DIRECTION,0,0,0);
+	alSourcef(source,AL_ROLLOFF_FACTOR,0);
+	alSourcei(source,AL_SOURCE_RELATIVE,AL_TRUE);
+	alSourcei(source,AL_LOOPING,AL_FALSE);
+
+    return source;
+}
+
+void saDestorySource(ALuint source)
+{
+    alDeleteSources(1,&source);
+}
+
+ALuint saCreateBuffer()
+{
+    int buffer;
+
+    alGenBuffers(1,&buffer);
+
+    return buffer;
+}
+
+void saDestoryBuffer(int buffer)
+{
+    alDeleteBuffers(1,&buffer);
+}
+
+void saLoadBuffer(int buffer,void *pData,unsigned int sz)
+{
+    alBufferData(buffer,AL_FORMAT_STEREO16,pData,sz,44100);
+}
+
+void saSetSourceLooping(int bLoop)
+{
+    if(bLoop)
+        alSourcei(myAudioSource,AL_LOOPING,AL_TRUE);
+    else
+        alSourcei(myAudioSource,AL_LOOPING,AL_FALSE);
+}
+
+void saPlaySound(ALuint source,ALuint buffer)
+{
+    alSourceStop(source);
+    alSourcei(source,AL_BUFFER,buffer);
+	alSourcePlay(source);
+}
+
--- SimpleAudio.h
+++ SimpleAudio.h
@@ -4,4 +4,6 @@
 #include <AL/al.h>
 #include <AL/alut.h>
 
+ALuint CreateAudioSource();                          //创建声音源
+
 #endif // SIMPLEAUDIO_H_INCLUDED
